8545743101a5fec45a8ba05d1e7d6eddecaf27fd
Samarpit Minz <samarpitminz@gmail.com>
2021-11-03 02:09:02 +0530

    redesigned game menu, new background and font

--- menu.py
+++ menu.py
@@ -14,17 +14,24 @@ from pygame.locals import (
 class Menu:
     def __init__(self, screen, network) -> None:
         SCREEN_WIDTH, SCREEN_HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
-        font = pygame.font.SysFont(name='systemfont', size=35) 
-        ready_text = font.render('READY', True, WHITE)
-        start_text = font.render('START', True, WHITE)
-        quit_text = font.render('QUIT', True, WHITE)
+        font = pygame.font.Font('font.ttf', 35) 
+        font_big = pygame.font.Font('font.ttf', 64) 
+        font_title = pygame.font.Font('font.ttf', 128) 
+        background = pygame.image.load('background.png')
+        background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        title_text = font_title.render('Multiplayer Pong', True, WHITE)
+        ready_text = font_big.render('READY', True, WHITE)
+        start_text = font_big.render('START', True, WHITE)
+        quit_text = font_big.render('QUIT', True, WHITE)
         print('in menu')
 
-        button_color = (170,170,170) 
-        button_x = 7*SCREEN_WIDTH/16
-        button_width = SCREEN_WIDTH/8
+        # button_color = (170,170,170) 
+        title_x = SCREEN_WIDTH/8
+        title_y = SCREEN_HEIGHT/8
+        button_x = 3*SCREEN_WIDTH/5
+        button_width = SCREEN_WIDTH/5
         button_height = SCREEN_HEIGHT/8
-        ready_y = SCREEN_HEIGHT/4
+        ready_y = SCREEN_HEIGHT/3
         start_y = ready_y + button_height + 20
         quit_y = start_y + button_height + 20
         clock = pygame.time.Clock()  
@@ -58,28 +65,31 @@ class Menu:
                             network.stop()
                             pygame.quit() 
 
-            screen.fill((60,25,60)) 
-            pygame.draw.rect(screen, button_color, [button_x, ready_y, button_width, button_height])
-            pygame.draw.rect(screen, button_color, [button_x, start_y, button_width, button_height])
-            pygame.draw.rect(screen, button_color, [button_x, quit_y, button_width, button_height])
-
+            screen.blit(background, (0, 0))
+            # pygame.draw.rect(screen, button_color, [button_x, ready_y, button_width, button_height])
+            # pygame.draw.rect(screen, button_color, [button_x, start_y, button_width, button_height])
+            # pygame.draw.rect(screen, button_color, [button_x, quit_y, button_width, button_height])
+            screen.blit(title_text, (title_x, title_y))
             screen.blit(ready_text, (button_x, ready_y))
             screen.blit(start_text, (button_x, start_y))
             screen.blit(quit_text, (button_x, quit_y))
+            server_status = network.client.connection.status - 1
+            if server_status < 3: 
+                server_status_text = font.render('Server ' + ['Disconnected', 'Connecting', 'Connected'][server_status], True, WHITE)
+                screen.blit(server_status_text, (7*SCREEN_WIDTH / 9, 9*SCREEN_HEIGHT / 10 - 50))
+                latency_text = font.render('Latency: ' + str(round(1000* network.client.connection.latency)) + ' ms', True, WHITE)
+                screen.blit(latency_text, (7*SCREEN_WIDTH / 9, 9*SCREEN_HEIGHT / 10))
 
-            latency_text = font.render('Latency: ' + str(round(1000* network.client.connection.latency)) + ' ms', True, WHITE)
-            screen.blit(latency_text, (8*SCREEN_WIDTH / 10, 8*SCREEN_HEIGHT / 10))
-
-            player_text_x = SCREEN_WIDTH / 10
-            player_text_y = SCREEN_HEIGHT / 10
+            player_text_x = SCREEN_WIDTH / 5
+            player_text_y = ready_y
             player_text_height = SCREEN_HEIGHT / 20
-            player_text_width = SCREEN_WIDTH / 5
+            # player_text_width = SCREEN_WIDTH / 5
             for i, player in enumerate(network.player_match_status):
                 if(player in [1, 2]):
-                    player_text = font.render('Player ' + str(i) + ['Disconnected', ' Connected', ' Ready'][player], True, BLACK)
-                    pygame.draw.rect(screen, WHITE, [player_text_x, player_text_y, player_text_width, player_text_height])
+                    player_text = font.render('Player ' + str(i) + [': Disconnected', ': Connected', ': Ready'][player], True, WHITE)
+                    # pygame.draw.rect(screen, WHITE, [player_text_x, player_text_y, player_text_width, player_text_height])
                     screen.blit(player_text, (player_text_x, player_text_y))
-                    player_text_y += (player_text_height + 20)
+                    player_text_y += (player_text_height + 10)
 
             pygame.display.flip()
             clock.tick(60)
